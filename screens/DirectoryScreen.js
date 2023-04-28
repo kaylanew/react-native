f1c06e03f202f11dd6de20e6bf72b15dd6ffebae
Kayla New <kayla.new.n@gmail.com>
2023-04-28 00:31:26 -0700

    Components Part 2

--- screens/DirectoryScreen.js
+++ screens/DirectoryScreen.js
@@ -4,7 +4,8 @@ import { Avatar, ListItem } from 'react-native-elements';
 const DirectoryScreen = (props) => {
     const renderDirectoryItem = ({ item: campsite }) => {
         return (
-            <ListItem>
+            <ListItem onPress={() => props.onPress(campsite.id)}>
+
                 <Avatar source={campsite.image} rounded />
                 <ListItem.Content>
                     <ListItem.Title>{campsite.name}</ListItem.Title>
@@ -14,8 +15,7 @@ const DirectoryScreen = (props) => {
                 </ListItem.Content>
             </ListItem>
         );
-    }
-
+    };
     return (
         <FlatList
             data={props.campsites}
